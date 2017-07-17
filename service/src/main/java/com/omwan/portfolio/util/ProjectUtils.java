31cab74e651be0cce66229bdc09a9461d9f275c2
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-07-16 21:10:51 -0400

    backend services, basic frontend skeleton

---
+++ service/src/main/java/com/omwan/portfolio/util/ProjectUtils.java
@@ -0,0 +1,25 @@
+package com.omwan.portfolio.util;
+
+import com.omwan.portfolio.domain.ProjectDTO;
+import com.omwan.portfolio.mongo.document.Project;
+
+import org.springframework.beans.BeanUtils;
+
+/**
+ * todo replace these with getter/setter calls
+ * Created by olivi on 07/16/2017.
+ */
+public class ProjectUtils {
+
+  public static ProjectDTO convertFromDocument(Project document) {
+    ProjectDTO domain = new ProjectDTO();
+    BeanUtils.copyProperties(document, domain);
+    return domain;
+  }
+
+  public static Project convertFromDomain(ProjectDTO domain) {
+    Project document = new Project();
+    BeanUtils.copyProperties(domain, document);
+    return document;
+  }
+}
---
+++ webapp/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by olivi on 07/16/2017.
+ */
+var app = angular.module("app", []);
