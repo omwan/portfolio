a1b90d1c740959a3b5cc1bc22096bce0fa7ca2c2
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-07-16 22:45:25 -0400

    adding project files

--- src/main/java/com/omwan/portfolio/Application.java
+++ src/main/java/com/omwan/portfolio/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortfolioApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ src/main/java/com/omwan/portfolio/mongo/document/Project.java
@@ -0,0 +1,128 @@
+package com.omwan.portfolio.mongo.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by olivi on 07/16/2017.
+ */
+@Document(collection = "project")
+public class Project {
+
+  @Id
+  private String id;
+  private String title;
+  private String category;
+  private String description;
+  private String notes;
+  private List<String> technologies;
+  private String github;
+  private String link;
+  private boolean isDeleted;
+  private boolean isPublic;
+  private Date startDate;
+  private Date endDate;
+
+  public Project() {
+
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
+
+  public List<String> getTechnologies() {
+    return technologies;
+  }
+
+  public void setTechnologies(List<String> technologies) {
+    this.technologies = technologies;
+  }
+
+  public String getGithub() {
+    return github;
+  }
+
+  public void setGithub(String github) {
+    this.github = github;
+  }
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+  public boolean isDeleted() {
+    return isDeleted;
+  }
+
+  public void setDeleted(boolean deleted) {
+    this.isDeleted = deleted;
+  }
+
+  public boolean isPublic() {
+    return isPublic;
+  }
+
+  public void setPublic(boolean aPublic) {
+    isPublic = aPublic;
+  }
+
+  public Date getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(Date startDate) {
+    this.startDate = startDate;
+  }
+
+  public Date getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(Date endDate) {
+    this.endDate = endDate;
+  }
+}
