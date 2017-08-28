87ec19617ed776347d7e6519cb10d4bfa7ecb008
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-08-28 11:49:27 -0400

    updating contact links

--- src/main/java/com/omwan/portfolio/config/SwaggerConfig.java
+++ src/main/java/com/omwan/portfolio/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.omwan.portfolio.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,6 +27,15 @@ public class SwaggerConfig {
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.ant("/api/**"))
+            .build()
+            .apiInfo(buildApiInfo());
+  }
+
+  private ApiInfo buildApiInfo() {
+    return new ApiInfoBuilder()
+            .contact(new Contact("Olivia Wan", "https://github.com/omwan", "wan.o@husky.neu.edu"))
+            .description("API documentation for Java/Spring REST services to manage data regarding my projects")
+            .title("Portfolio Project")
             .build();
   }
 }
